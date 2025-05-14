b687071a04504bb01f2084c7c96727c7283194fc
Owen Parry <owen.parry@ukaea.uk>
2025-05-14 11:53:09 +0100

    Add py-xhermes as an optional dependency of hermes-3 and rejig other dependencies.

--- packages/hermes-3/package.py
+++ packages/hermes-3/package.py
@@ -3,7 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
+from spack import *
+
 
 class Hermes3(CMakePackage):
     """A multifluid magnetized plasma simulation model.
@@ -27,19 +28,44 @@ class Hermes3(CMakePackage):
     version("1.2.1", commit="f1dadcc")
     version("1.2.0", commit="081c8cf")
 
-    variant("petsc", default=False, description="Enable PETSc solvers")
     variant(
-        "limiter", default="MC", description="Slope limiter", values=("MC", "MinMod"), multi=False
+        "limiter",
+        default="MC",
+        description="Slope limiter",
+        values=("MC", "MinMod"),
+        multi=False,
+    )
+    variant("petsc", default=False, description="Builds with PETSc support.")
+    variant("sundials", default=True, description="Builds with SUNDIALS support.")
+    variant(
+        "xhermes", default=True, description="Builds xhermes (required for some tests)."
     )
 
-    depends_on("cmake", type="build")
-    depends_on("mpi")
-    depends_on("fftw")
-    depends_on("netcdf-cxx4")
-    depends_on("petsc+hypre+mpi", when="+petsc")
+    # Always-required dependencies
+    # depends_on("adios2", type=("build", "link", "run"))
+    depends_on("cmake@3.24:", type="build")
+    depends_on("fftw", type=("build", "link", "run"))
+    depends_on("mpi", type=("build", "link", "run"))
+    depends_on("netcdf-cxx4", type=("build", "link", "run"))
+    # Should these be xhermes deps instead?
+    depends_on("py-cython", type=("build", "link", "run"))
+    depends_on("py-jinja2", type=("build", "link", "run"))
+    depends_on("py-netcdf4", type=("build", "link", "run"))
+
+    # Variant-controlled dependencies
+    depends_on(
+        "petsc+hypre+mpi~debug~fortran", when="+petsc", type=("build", "link", "run")
+    )
+    depends_on("py-xhermes", when="+xhermes", type=("build", "link", "run"))
+    # Could add Sundials as a spack dependency here?
+    # Download it via the BOUT cmake flag for now
+    # depends_on("sundials", when="+sundials", type=("build", "link", "run"))
 
     def cmake_args(self):
+        # Always build with Sundials.
+        # Use variants to toggle other config options.
         return [
             self.define("BOUT_DOWNLOAD_SUNDIALS", True),
+            self.define_from_variant("BOUT_USE_PETSC", "petsc"),
             self.define_from_variant("HERMES_SLOPE_LIMITER", "limiter"),
         ]
