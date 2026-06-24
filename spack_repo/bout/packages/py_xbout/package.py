5e0cb76c86f4fcbe992be9cf7bcaf8e551c1abf9
Owen Parry <owen.parry@ukaea.uk>
2026-06-24 15:06:27 +0100

    Add intermediate py-xbout versions, to enforce hdf5/netcdf version requirements.

--- spack_repo/bout/packages/py_xbout/package.py
+++ spack_repo/bout/packages/py_xbout/package.py
@@ -23,6 +23,22 @@ class PyXbout(PythonPackage):
         "0.3.7",
         sha256="51b6bcc888553037a623f68dccfe7755ca409801d5b2dd1b8b1ecaca78c1eff1",
     )
+    version(
+        "0.3.8",
+        sha256="9d17f3425b46304d837dff514b3d1541f85e5de69eaa43629c1806220b4a0b26",
+    )
+    version(
+        "0.4.0",
+        sha256="1a118823550e5db0ec239bee2f61a55545eb1643035082e5d4919aa5390cc847")
+
+
+    # Treat intermediate versions, mapped to specific Git commits, as release candidates ('rc' suffixes)
+    #  - Can be used internally or by consuming packages when inter-release changes break something
+    # Format:
+    #   version("<next_release_version>rc<date_in_YYYYMMDD>", commit="<git_hash>")
+    version("0.4.0rc20250925", commit="9c634a4492cd480f9883151b4f89b9d22f607727") # netcdf4=>h5netcdf
+    version("0.4.0rc20260211", commit="afac4967c662e1c75b549bf585e15a6330548d8c") # h5py
+
 
     # Point at latest master/main branch
     version("master", branch="main")
@@ -37,12 +53,14 @@ class PyXbout(PythonPackage):
 
     # Runtime dependencies
     depends_on("py-animatplot-ng@0.4.2:", type=("build", "run"))
-    depends_on("py-boutdata@0.1.4:", type=("build", "run"))
+    #depends_on("py-boutdata@0.1.4:", type=("build", "run"))
     depends_on("py-dask@2.10.0:+array", type=("build", "run"))
     depends_on("py-gelidum@0.5.3:", type=("build", "run"))
+    depends_on("py-h5py", type=("build", "run"), when="@0.4.0rc20260211:")
+    depends_on("py-h5netcdf", type=("build", "run"), when="@0.4.0rc20250925:")
     depends_on("py-matplotlib@3.3.3:", type=("build", "run"))
     depends_on("py-natsort@5.5.0:", type=("build", "run"))
-    depends_on("py-netcdf4@1.4.0:", type=("build", "run"))
+    depends_on("py-netcdf4@1.4.0:", type=("build", "run"), when="@:0.3.8")
     depends_on("py-pillow@6.1.0:", type=("build", "run"))
     depends_on("py-xarray@2023.01.0:", type=("build", "run"))
 
