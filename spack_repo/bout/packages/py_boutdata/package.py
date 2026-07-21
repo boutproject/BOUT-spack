51c725e5ac840c8c2b22560f720f9d26bbb97a2e
Owen Parry <owen.parry@ukaea.uk>
2026-07-21 11:06:13 +0100

    Make py-boutdata, py-xbout and py-xhermes pure git packages and standardise the code used to add tagged versions.

--- spack_repo/bout/packages/py_boutdata/package.py
+++ spack_repo/bout/packages/py_boutdata/package.py
@@ -6,21 +6,35 @@ class PyBoutdata(PythonPackage):
     """Python tools for working with BOUT++."""
 
     homepage = "https://github.com/boutproject/boutdata"
-    pypi = "boutdata/boutdata-0.2.1.tar.gz"
+    git = "https://github.com/boutproject/boutdata.git"
 
     # Set a maintainer if submitting this package to the spack repo
     # maintainers("github_user1", "github_user2")
 
     license("LGPL-3.0")
 
-    version(
-        "0.3.0",
-        sha256="d76dd6fc1f5d916006a5e5d6db9b2835f5b5eb94c675169fd2529fc36c820323"
-    )
-    version(
+    # Release version git tags
+    tagged_versions = [
+        "0.1.5",
+        "0.1.6",
+        "0.1.7",
+        "0.1.8",
+        "0.1.9",
+        "0.1.10",
+        "0.2.0",
         "0.2.1",
-        sha256="043cddaeb38b128d2525f2005f48a5b7717ff5832a932183a4bef1d3eae389e0",
-    )
+        "0.3.0",
+        "0.4.0",
+    ]
+    for v in tagged_versions:
+        version(v, tag=f"v{v}")
+
+    # Treat intermediate versions, mapped to specific Git commits, as release candidates ('rc' suffixes)
+    #  - Can be used internally or by consuming packages when inter-release changes break something
+    #  - By convention, commit hashes point to master; i.e. the commit where the breaking change was merged in
+    #  - If the next release version isn't known - increment the last release version by 0.0.1
+    # Format (don't change the line below, as it is used in CI to update package versions!)
+    #   version("<next_release_version>rc<date_in_YYYYMMDD>", commit="<git_hash>")
 
     # Compatible Python versions
     depends_on("python@3.9:", type=("build", "run"))
